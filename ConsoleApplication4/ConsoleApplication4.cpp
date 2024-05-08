926efb5f5d27e506773ba8155fb219d1c96fbfef
jeong <jeong@DESKTOP>
2024-05-08 23:46:52 +0900

    초기 커밋.

---
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int Grid[10][10] = {};
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			cin >> Grid[i][j];
+		}
+	}
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			if (Grid[i][j] == 0) {
+				if (Grid[i][j - 1] == 2 || Grid[i][j + 1] == 2 || Grid[i][j - 1] == 1 || Grid[i][j + 1] == 1 || Grid[i - 1][j] == 2 || Grid[i + 1][j] == 2 || Grid[i - 1][j] == 1 || Grid[i + 1][j] == 1) {
+					Grid[i][j] = 9;
+				}
+			}
+		}
+	}
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			cout << Grid[i][j];
+			if (j != 9)cout << " ";
+		}
+		if (i != 9)cout << "\n";
+	}
+}
